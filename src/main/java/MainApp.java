fe8616e3dd1067a60d067f0cb4dca6ad2d5dae49
Amanda Huang <79762895+AmandaJ-Huang@users.noreply.github.com>
2021-12-17 18:09:24 -0500

    added formula and unit tests-even, odd, negative, bignums, thirtynums, allnegatives

--- src/main/java/MainApp.java
+++ src/main/java/MainApp.java
@@ -1,13 +1,11 @@
 public class MainApp {
-    public static void main(String[] args) {
 
-    }
-
-    public Integer solveWithMath(Integer[] input) {
+    public static Integer solveWithMath(Integer[] input) {
         Integer sum = 0;
+        Integer multiplier = input.length;
 
         for(int i = 0; i < input.length; i++) {
-            sum += input[i] * i;
+            sum += (input[i] * (i+1) * (multiplier-i));
         }
 
         return sum;
--- src/test/java/MainAppTest.java
+++ src/test/java/MainAppTest.java
@@ -1,2 +1,85 @@
+import org.junit.Assert;
+import org.junit.Test;
+
 public class MainAppTest {
+
+    @Test
+    public void testInputOfEvenLength() {
+        // Given
+        Integer[] input = {1, 3, 5, 4};
+        Integer expected = 68;
+
+        // When
+        Integer actual = MainApp.solveWithMath(input);
+
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testInputOfOddLength() {
+        // Given
+        Integer[] input = {1, 3, 5, 4, 7};
+        Integer expected = 141;
+
+        // When
+        Integer actual = MainApp.solveWithMath(input);
+
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testInputWithNegative() {
+        // Given
+        Integer[] input = {1, -3, 5, 4};
+        Integer expected = 32;
+
+        // When
+        Integer actual = MainApp.solveWithMath(input);
+
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testInputOfBigNums() {
+        // Given
+        Integer[] input = {1159881, 1366082, 1489909, 1767619, 1963582};
+        Integer expected = 54096104;
+
+        // When
+        Integer actual = MainApp.solveWithMath(input);
+
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testInputOfThirtyNums() {
+        // Given
+        Integer[] input = {28, 32, 64, 82, 63, 2, 59, 80, 83, 93, 56, 94, 87, 79,
+                16, 11, 68, 18, 99, 8, 17, 65, 89, 52, 25, 91, 49, 13, 43, 24};
+        Integer expected = 276864;
+
+        // When
+        Integer actual = MainApp.solveWithMath(input);
+
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testInputOfAllNegativesAndOddLength(){
+        // Given
+        Integer[] input = {-954, -272, -202, -739, -703, -792, -249, -669, -462, -718, -724};
+        Integer expected = -163074;
+
+        // When
+        Integer actual = MainApp.solveWithMath(input);
+
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
+
 }
